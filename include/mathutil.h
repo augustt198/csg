d35b229a942db6901369c88753a843276e6a1b2b
August <augustt198@gmail.com>
2017-07-25 18:45:23 -0400

    small change

--- include/mathutil.h
+++ include/mathutil.h
@@ -15,13 +15,13 @@ float mix(float a, float b, float t) {
 }
 
 // thanks to Inigo Quilez
-float smoothmin(float a, float b, float k=0.1){ 
+float smoothmin(float a, float b, float k=0.05){ 
     float h = clamp(0.5 + 0.5*(b - a)/k, 0.0, 1.0);
     return mix(b, a, h) - k*h*(1.0 - h);
 }
 
 // min(x, y) = -max(-x, -y)
-float smoothmax(float a, float b, float k=0.1) {
+float smoothmax(float a, float b, float k=0.05) {
     return -smoothmin(-a, -b, k);
 }
 
